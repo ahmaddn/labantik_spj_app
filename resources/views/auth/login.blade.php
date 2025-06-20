234a4d590749e6897b7bbf095842f654ae3db516
ahmaddn <najmyahmadmaulana@gmail.com>
2025-06-20 18:48:07 +0700

    [fix] rendering content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,59 @@
 @extends('layouts.master')
 @section('content')
-    <div class="main-wrapper login-body">
-        <div class="login-wrapper">
-            <div class="container">
-                <div class="loginbox">
-                    <div class="login-left">
-                        <img class="img-fluid" src="assets/img/login.png" alt="Logo">
-                    </div>
-                    <div class="login-right">
-                        <div class="login-right-wrap">
-                            <h1>Welcome to Spj App</h1>
-                            <p class="account-subtitle">Need an account? <a href="register">Sign Up</a></p>
-                            <h2>Sign in</h2>
-                            @if (session('success'))
-                                <div class="alert alert-success alert-dismissible fade-show">
-                                    {{ session('success') }}
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                        aria-label="Close"></button>
-                                </div>
-                            @endif
 
-                            @if ($errors->any())
-                                <div class="alert alert-danger alert-dismissible fade-show">
-                                    <ul class="mb-0">
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                        aria-label="Close"></button>
-                                </div>
-                            @endif
-                            <form action="{{ route('login') }}" method="post">
-                                @csrf
-                                <div class="form-group">
-                                    <label>Username <span class="login-danger">*</span></label>
-                                    <input class="form-control" type="text" name="username"
-                                        value="{{ old('username', request()->cookie('remember_username')) }}">
-                                    <span class="profile-views"><i class="fas fa-user-circle"></i></span>
-                                </div>
-                                <div class="form-group">
-                                    <label>Password <span class="login-danger">*</span></label>
-                                    <input class="form-control pass-input" type="password" name="password"
-                                        value="{{ old('password', request()->cookie('remember_password')) }}">
-                                    <span class="profile-views feather-eye-off toggle-password"
-                                        style="cursor: pointer"></span>
-                                </div>
-                                <div class="forgotpass">
-                                    <div class="remember-me">
-                                        <label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
-                                            <input name="remember_me" type="checkbox" name="radio">
-                                            <span class="checkmark"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <button class="btn btn-primary btn-block" type="submit">Login</button>
-                                </div>
-                            </form>
+    <div class="container">
+        <div class="loginbox">
+            <div class="login-left">
+                <img class="img-fluid" src="assets/img/login.png" alt="Logo">
+            </div>
+            <div class="login-right">
+                <div class="login-right-wrap">
+                    <h1>Welcome to Spj App</h1>
+                    <p class="account-subtitle">Need an account? <a href="register">Sign Up</a></p>
+                    <h2>Sign in</h2>
+                    @if (session('success'))
+                        <div class="alert alert-success alert-dismissible fade-show">
+                            {{ session('success') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
-                    </div>
+                    @endif
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger alert-dismissible fade-show">
+                            <ul class="mb-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+                    <form action="{{ route('login') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label>Username <span class="login-danger">*</span></label>
+                            <input class="form-control" type="text" name="username"
+                                value="{{ old('username', request()->cookie('remember_username')) }}">
+                            <span class="profile-views"><i class="fas fa-user-circle"></i></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Password <span class="login-danger">*</span></label>
+                            <input class="form-control pass-input" type="password" name="password"
+                                value="{{ old('password', request()->cookie('remember_password')) }}">
+                            <span class="profile-views feather-eye-off toggle-password" style="cursor: pointer"></span>
+                        </div>
+                        <div class="forgotpass">
+                            <div class="remember-me">
+                                <label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
+                                    <input name="remember_me" type="checkbox" name="radio">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-primary btn-block" type="submit">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
