fa6ade2841a04739fb76c410b2b57ab5692ef7f9
ahmaddn <najmyahmadmaulana@gmail.com>
2025-06-20 14:55:12 +0700

    [add] fungsi softdeletes di semua model kecuali user

--- app/Models/Bendahara.php
+++ app/Models/Bendahara.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bendahara extends Model
 {
+    use SoftDeletes;
     //
     protected $table = 'bendahara';
     protected $primaryKey = 'id';
--- app/Models/Kegiatan.php
+++ app/Models/Kegiatan.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kegiatan extends Model
 {
+    use SoftDeletes;
     //
     protected $table = 'kegiatan';
     protected $primaryKey = 'id';
